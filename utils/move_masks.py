c96422b295b11abf867a9d1add912ca62c9a425e
ProbablyNotBrandon <brandonthiessen2005@gmail.com>
2025-07-15 12:30:17 -0400

    Started rook and bishop precomputed move masks

--- utils/move_masks.py
+++ utils/move_masks.py
@@ -20,8 +20,7 @@ def init_all():
 
 def init_knight_move_masks():
     def generate_knight_move_mask(bit):
-        file = bit % 8
-        rank = bit // 8
+        file, rank = bit_to_fr(bit)
 
         deltas = [
             (1, 2), (-1, 2), (1, -2), (-1, -2),
@@ -41,6 +40,25 @@ def init_knight_move_masks():
     print(KNIGHT_MOVE_MASKS)
 
 
+def init_rook_move_masks():
+    def generate_rook_move_mask(bit):
+        file, rank = bit_to_fr(bit)
+        mask = np.uint64(0)
+        for i in range(8):
+            mask |= (np.uint64(1) << np.uint64(file + i * 8))
+        for i in range(8):
+            mask |= (np.uint64(1) << np.uint64(rank * 8 + i))
+        return mask
+
+    for bit in range(64):
+        ROOK_MOVE_MASKS[bit] = generate_rook_move_mask(bit)
+    print(ROOK_MOVE_MASKS)
+
+
+def init_pawn_move_masks():
+    pass
+
+
 def fr_to_bit(file, rank):
     return rank * 8 + file
 
@@ -50,15 +68,21 @@ def bit_to_fr(bit):
 
 
 def init_bishop_move_masks():
-    pass
+    def generate_bishop_move_mask(bit):
+        file, rank = bit_to_fr(bit)
+        mask = np.uint64(0)
+        deltas = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
+        for df, dr in deltas:
+            for mul in range(1, 7):
+                f = file + df * mul
+                r = rank + dr * mul
+                if 0 <= f <= 7 and 0 <= r <= 7:
+                    mask |= (np.uint64(1) << np.uint64(fr_to_bit(f, r)))
+        return mask
 
-
-def init_rook_move_masks():
-    pass
-
-
-def init_pawn_move_masks():
-    pass
+    for bit in range(64):
+        BISHOP_MOVE_MASKS[bit] = generate_bishop_move_mask(bit)
+    print(BISHOP_MOVE_MASKS)
 
 
 if __name__ == "__main__":
